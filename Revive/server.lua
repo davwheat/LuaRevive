feebfd9c615a28717f85ab3975f35c770245103b
davwheat <blokmaster.help@gmail.com>
2018-02-26 21:07:11 +0000

    Attempting fix

--- Revive/server.lua
+++ Revive/server.lua
@@ -3,19 +3,11 @@ AddEventHandler(
   function(source, name, message)
     if string.starts(message, "/revive") then
       CancelEvent()
-      -- if string.sub(message, 1, 8) ~= nil then
-      --   if tonumber(string.sub(message, 9)) then
-      --     if NetworkDoesNetworkIdExist(tonumber(string.sub(message, 9))) then
-      --       TriggerClientEvent("reviveOther", string.sub(message, 9))
-      --     else
-      --       TriggerEvent("chatMessage", "Revive | ", {244, 67, 54}, "Invalid player ID")
-      --     end
-      --   else
-      --     TriggerEvent("chatMessage", "Revive | ", {244, 67, 54}, "Invalid player ID")
-      --   end
-      -- else
+      if string.sub(message, 1, 8) ~= nil then
+        TriggerClientEvent("reviveOther", string.sub(message, 9))
+      else
       TriggerClientEvent("reviveSelf", source)
-      -- end
+      end
     end
   end
 )
